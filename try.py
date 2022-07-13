f68825478e3a454f49a5b016610bda233b99ecbd
guille2400 <guillermo.sanchez@utec.edu.pe>
2022-07-12 22:14:59 -0500

    done todo

--- try.py
+++ try.py
@@ -3,29 +3,20 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 mensajes ={}
-
-# Testing Route
-#@app.route('/ping', methods=['GET'])
-#def ping():
-#    return jsonify({'response': 'pong!'})
-
-# Get Data Routes
 @app.route('/allmensajes')
-def getProducts():
-    # return jsonify(products)
+def get_Products():
     return jsonify({'mensajes': mensajes})
 
 
 @app.route('/mensaje/<string:x>',methods=['GET'])
-def getProduct(x):
+def get_Product(x):
 	if x in mensajes:
 		return jsonify(mensajes[x])
 	else:
          return jsonify([{'status': 'No hay mensaje'}])
 
-# Create Data Routes
 @app.route('/mensaje', methods=['POST'])
-def sentProduct():
+def sent_Product():
     try:
             mensaje: request.json["mensaje"]
             topic = request.json['topic']
@@ -33,17 +24,6 @@ def sentProduct():
             return jsonify({"status":"ok"})
     except:
             return jsonify({"status":"fail"})
-
-# DELETE Data Route
-#@app.route('/products/<string:product_name>', methods=['DELETE'])
-#def deleteProduct(product_name):
-#    productsFound = [product for product in products if product['name'] == product_name]
-#    if len(productsFound) > 0:
-#        products.remove(productsFound[0])
-#        return jsonify({
-#            'message': 'Product Deleted',
-#            'products': products
-#        })
 
 
 if __name__ == '__main__':
